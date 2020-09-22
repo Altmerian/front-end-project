33d05a450d8165d0d447c7dec1ec7e34f3b20b82
Pavel Shakhlovich <Altmersc@gmail.com>
2020-09-22 14:08:54 +0300

    jenkins-aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-18-192-11-214.eu-central-1.compute.amazonaws.com:8087/gift-rest-service'
+  apiUrl: 'http://ec2-18-194-238-253.eu-central-1.compute.amazonaws.com:8087/gift-rest-service'
 };
